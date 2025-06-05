0181ef043f8426bece3ee35c4ea1f3ca1f2db934
ihawp <ihawpebay@gmail.com>
2025-06-05 01:34:11 -0600

    Blog validate completed*

--- middleware/blogValidate.js
+++ middleware/blogValidate.js
@@ -1,36 +1,17 @@
 const { body } = require('express-validator');
 
-/*
-
-    Intake a blog post form.
-
-    Structure of form:
-
-        Title
-
-        Featured Image
-
-        Content: Use [*] to target items within the array
-                    so that those items may be checked for
-                    proper type (string)
-
-        Tags
-  
-        
-
-*/
-
 const blogValidate = [
-    body('title')
+    body(['title', 'author'])
         .isString()
-        .withMessage('Title must not be empty.'),
+        .isLength({ min: 1, max: 255 })
+        .withMessage('Value must not be empty.'),
     body('content')
         .isArray()
         .isLength({ min: 1 })
-        .withMessage('Content must be in array format'),
-    body('content[*]')
+        .withMessage('Content must be in array format.'),
+    body(['content[*]', 'tags[*]'])
         .isString()
-        .withMessage(''),
+        .withMessage('Array contents must be in string format.'),
 ];
 
 module.exports = blogValidate;
