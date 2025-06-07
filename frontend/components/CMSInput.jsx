960cba90f032baac8e61e732270a7bb1e3d4133a
ihawp <ihawpebay@gmail.com>
2025-06-07 03:56:05 -0600

    added component CMSInput for reuse of functionality, planning to alter functionality with new components, for example I want the tools used to be a list of checkboxes containing svgs (that are the possible icons) rather than entering the icon names manually, that is not possible using the current component. In the grand scheme these will just be components that are available to use across the CMS* for whatever case it may be, for the example provided, an example of the reusability could be the svg list being an object or array passed to the component

---
+++ frontend/components/CMSInput.jsx
@@ -0,0 +1,40 @@
+function CMSInput({ handleAdd, handleClear, handleRemove, handleChange, sectionTitle, sectionName, form }) {
+    return <div className="flex flex-col bg-[#222] bw-1 bs-solid my-1 p-4 rounded-[8px]">
+        <div className="flex flex-row items-center justify-between">
+            <h2 className="text-xl">{sectionTitle}</h2>
+            <div className="flex flex-row gap-2">
+                <button className="w-max p-2 bg-green-700 cursor-pointer rounded" type="button" onClick={handleAdd} name={sectionName} value={sectionName}>
+                    + Add
+                </button>
+                <button className="w-max p-2 bg-red-600 cursor-pointer rounded" type="button" onClick={handleClear} name={sectionName} value={sectionName}>
+                    X Clear All
+                </button>
+            </div>
+        </div>
+        
+        <div className="my-4">
+            {form[sectionName].length > 0 ? form[sectionName].map((item, key) => {
+                return <div className="flex flex-row items-center mt-2" key={item.id}>
+                    <textarea className='p-1 w-full h-[84px]'
+                        name={sectionName}
+                        id={item.id} // Unique id for each input
+                        placeholder={sectionTitle}
+                        value={item.value} // Bind to the value of the object in the array
+                        onChange={handleChange} // Handle the change for this specific item
+                        maxLength={255}
+                    ></textarea>
+                    <button 
+                        type="button"
+                        name={sectionName}
+                        onClick={(e) => handleRemove(e, item.id)}
+                        className="bg-red-600 cursor-pointer rounded-[8px] p-2 h-max w-[130px] ml-6"
+                    >
+                        - Remove
+                    </button>
+                </div>
+            }) : null}
+        </div>
+    </div>
+}
+
+export default CMSInput;
--- frontend/components/PortfolioForm.jsx
+++ frontend/components/PortfolioForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import CMSInput from './CMSInput';
 
 function PortfolioForm() {
 
@@ -107,69 +108,30 @@ function PortfolioForm() {
         }
     };
 
-    // the form needs to be dynamically generatable
-    // I need to be able to click add new key task so that I may have another array entry..?
-    // I believe that is the most straightforward option, it would be less ideal to spend time splitting by commas (,) 
-    // and having extra formatting, multiple text box values added to array seems reasonable
-
     return <form onSubmit={submitPortfolioEntry} encType="multipart/form-data" className='flex flex-col w-180'>
             <input type="text" name="title" id="title" placeholder="Title" value={form.title} onChange={handleChange} required />
             <input type="text" name="intro" id="intro" placeholder="Intro" value={form.intro} onChange={handleChange} required />
             <input type="text" name="role" id="role" placeholder="Role" value={form.role} onChange={handleChange} />
-            <textarea name="timeline" id="timeline" placeholder="Timeline" value={form.timeline} onChange={handleChange}></textarea>
-            <textarea name="toolsUsed" id="toolsUsed" placeholder="Tools Used" value={form.toolsUsed} onChange={handleChange}></textarea>
 
-            <div aria-label="Add Skills Applied Section" className="flex flex-col bg-[#222] bw-1 bs-solid my-12 p-4 rounded-[8px]">
+            <CMSInput handleAdd={handleAdd} handleChange={handleChange} handleClear={handleClear} handleRemove={handleRemove} sectionTitle="Skills Applied" sectionName="skillsApplied" form={form}/>
 
-                <div className="flex flex-row items-center justify-between">
+            <CMSInput handleAdd={handleAdd} handleChange={handleChange} handleClear={handleClear} handleRemove={handleRemove} sectionTitle="Key Tasks" sectionName="keyTasks" form={form}/>
 
-                    <h2 className="text-xl">Skills Applied</h2>
+            <CMSInput handleAdd={handleAdd} handleChange={handleChange} handleClear={handleClear} handleRemove={handleRemove} sectionTitle="Tools Used" sectionName="toolsUsed" form={form}/>
 
-                    <div className="flex flex-row gap-2">
-                        <button className="w-max p-2 bg-green-700 cursor-pointer rounded" type="button" onClick={handleAdd} name="skillsApplied" value="skillsApplied">
-                            + Add
-                        </button>
-                        <button className="w-max p-2 bg-red-600 cursor-pointer rounded" type="button" onClick={handleClear} name="skillsApplied" value="skillsApplied">
-                            X Clear All
-                        </button>
-                    </div>
-                </div>
-                
-                <div className="my-4">
-                    {form.skillsApplied.length > 0 ? form.skillsApplied.map((item, key) => {
-                        return <div className="flex flex-row items-center mt-2">
-                            <textarea className='p-1 w-full h-[84px]'
-                                key={item.id} // Use item.id as the key
-                                name="skillsApplied"
-                                id={item.id} // Unique id for each input
-                                placeholder="Skills Applied"
-                                value={item.value} // Bind to the value of the object in the array
-                                onChange={handleChange} // Handle the change for this specific item
-                                maxLength={255}
-                            ></textarea>
-                            <button 
-                                type="button"
-                                name="skillsApplied"
-                                onClick={(e) => handleRemove(e, item.id)}
-                                className="bg-red-600 cursor-pointer rounded-[8px] p-2 h-max w-[130px] ml-6"
-                            >
-                                - Remove
-                            </button>
-                        </div>
-                    }) : null}
-                </div>
-            </div>
+            <CMSInput handleAdd={handleAdd} handleChange={handleChange} handleClear={handleClear} handleRemove={handleRemove} sectionTitle="Timeline" sectionName="timeline" form={form}/>
 
-            <textarea name="keyTasks" id="keyTasks" placeholder="Key Tasks" value={form.keyTasks} onChange={handleChange}></textarea>
-            <textarea name="challenges" id="challenges" placeholder="Challenges" value={form.challenges} onChange={handleChange}></textarea>
-            <textarea name="takeaways" id="takeaways" placeholder="Takeaways" value={form.takeaways} onChange={handleChange}></textarea>
+            <CMSInput handleAdd={handleAdd} handleChange={handleChange} handleClear={handleClear} handleRemove={handleRemove} sectionTitle="Challenges" sectionName="challenges" form={form}/>
+
+            <CMSInput handleAdd={handleAdd} handleChange={handleChange} handleClear={handleClear} handleRemove={handleRemove} sectionTitle="Takeaways" sectionName="takeaways" form={form}/>
+
             <textarea name="solutionSummary" id="solutionSummary" placeholder="Solution Summary" value={form.solutionSummary} onChange={handleChange}></textarea>
             <input type="url" name="githubURL" id="githubURL" placeholder="GitHub URL" value={form.githubURL} onChange={handleChange} />
             <input type="url" name="projectSite" id="projectSite" placeholder="Project Site" value={form.projectSite} onChange={handleChange} />
 
             <input type="file" name="files" id="files" multiple onChange={handleFileChange} />
 
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" className="cursor-pointer" />
         </form>
 }
 
